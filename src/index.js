064edfccdf8dbc875521616dfc485c6c5b61073b
Kenyatta Hill <khill@insight2profit.com>
2020-07-29 20:35:11 -0400

    fix server route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require('path')
 const routes = require("./routes");
-const PORT = process.env.PORT || 3002 
-const app = express() 
+const PORT = process.env.PORT || 3002
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,17 +12,18 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("frontend/build"));
 }
 app.use(routes)
-// app.get("*", (request, response) => {
-//     response.sendFile(path.join(__dirname, "./frontend/build/index.html"));
-// });
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/exerciseApp", {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
 const server = app.listen(PORT, () => {
     console.log(`API server now listening on port ${PORT}`)
-}); 
+});
 
 module.exports = server

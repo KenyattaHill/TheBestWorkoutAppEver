eef8ab6402360c9f15582bd5560540b4b17cc74d
Kenyatta Hill <khill@insight2profit.com>
2020-08-11 11:59:05 -0400

    fix the color red

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
-import './index.css';
 import 'semantic-ui-less/semantic.less'
+import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
